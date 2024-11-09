d81d546837b144590bd8fb886eaa9d32de350d04
ayshu31 <ayshu7794@gmail.com>
2024-11-09 21:17:18 +0530

    Add files via upload

--- pythoninterncalculator.py
+++ pythoninterncalculator.py
@@ -2,20 +2,20 @@ m=int(input("enter a number"))
 n=int(input("enter another number"))
 op=input("enter your choice of operation")
 if op=="addition":
-    print(m+n)
+    print("the calculated value is: ",m+n)
 elif op=="subtraction":
-    print(m-n)
+    print("the calculated value is: ",m-n)
 elif op=="multiplication":
-    print(m*n)
+    print("the calculated value is: ",m*n)
 elif op=="division":
-    print(m/n)
+    print("the calculated value is: ",m/n)
 elif op=="floor division":
-    print(m//n)
+    print("the calculated value is: ",m//n)
 elif op=="remainder":
-    print(m%n)
+    print("the calculated value is: ",m%n)
 elif op=="exponentiation":
-    print(m**n)
-print("the calculated value is: ",op)
-    
+    print("the calculated value is: ",m**n)
+
+
 
 
